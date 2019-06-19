a93d8905cfa8e97bdf9df8d398d124d615f6af69
Johnny Wilkinson <john.wilkinson@physics.ox.ac.uk>
2019-06-19 12:07:21 +0100

    Created MANTID fit function (which simply linearly interpolates between precalculated values

---
+++ MantidFitFunction.py
@@ -0,0 +1,56 @@
+# MantidFitFunction.py - a fit function for DecoherenceCalculator for use in MANTID.
+#
+# Created 19/6/19, John Wilkinson
+
+from mantid.api import *
+import numpy as np
+
+
+# You choose which type you would like by picking the super class
+class DecoherenceFunction(IFunction1D): # or IPeakFunction
+
+    def category(self):
+        return 'Muon'
+
+    def init(self):
+        # register initial amplitude A
+        self.declareParameter("A", 1.0)
+
+        # open the file, and load the data into numpy arrays
+        self.time_data, self.amplitude_data = self.importGLEdata("/Users/johnny/Documents/University/Decoherence_calculator/CaF2data.dat")
+
+        # normalise by making amplitude 1 (for now - multiply by A in the actual function)
+        normalise_factor = 1/self.amplitude_data[0]
+
+
+    def function1D(self, xvals):
+        # linearly interpolate between points to get the function value
+        A = self.getParameterValue("A")
+        return A*np.interp(xvals, self.time_data, self.amplitude_data, 0, 0)
+
+    def importGLEdata(self, file_location):
+        # open the file
+        GLE_data_file = open(file_location, "r")
+
+        time_data = []
+        amplitude_data = []
+
+        # for each line in the file...
+        for file_line in GLE_data_file:
+            # check the line doesnt start with ! (comment in GLE)
+            if file_line[0] != "!":
+                # read in the data (if we cant convert, pass)
+                try:
+                    output_data = [float(entry) for entry in file_line.split()]
+                    time_data.append(output_data[0])
+                    amplitude_data.append(output_data[1])
+                except ValueError:
+                    pass
+
+        # convert into numpy arrays
+        time_data = np.array(time_data)
+        amplitude_data = np.array(amplitude_data)
+        return time_data, amplitude_data
+
+# Register with Mantid
+FunctionFactory.subscribe(DecoherenceFunction)
