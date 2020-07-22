e69d8d43ec8d2538d66d12b29667e94b05e47e48
Johnny Wilkinson <johnnywilkinson12358@gmail.com>
2020-07-22 14:31:11 +0100

    fixed typo

--- MDecoherenceAtom.py
+++ MDecoherenceAtom.py
@@ -197,7 +197,7 @@ nucleon_properties = {
            "abundance": 1,
            "Q": 0.149,
            "charge": +3,
-           "anti_shielding": 2.3}
+           "anti_shielding": 2.3},
     "Sc": {"II": 7,
            "gyromag_ratio": 64.9895,
            "abundance": 1,
