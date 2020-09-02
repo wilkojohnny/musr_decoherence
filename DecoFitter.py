baa13426908becfab204bd63d94cba12500aca14
Johnny Wilkinson <john.wilkinson@physics.ox.ac.uk>
2020-09-02 16:48:42 +0100

    Can now read in forward and backward data separately, so that alpha can be changed easily

--- DecoFitter.py
+++ DecoFitter.py
@@ -18,10 +18,11 @@ except ModuleNotFoundError:
 from lmfit import *  # for nls curve fitting
 
 
-def fit(data_file_location: str, fit_function, params: Parameters, plot: bool, start_time=None, end_time=None,
-        just_plot=False, outfile_location=None, algorithm='leastsq', epsfcn=1e-4):
+def fit(muon_data: dict, fit_function, params: Parameters, plot: bool, start_time=None,
+        end_time=None, just_plot=False, outfile_location=None, algorithm='leastsq', epsfcn=1e-4):
     """
-    :param data_file_location: location of the muon data file
+    :param muon_data: dict with keys N_F and N_B, the location of the files containing the forward and backward counts,
+                      and alpha, OR key 'asymmetry' with the location of the file containing the full asymmetry data
     :param fit_function: function to be fitted
     :param params: fit parameters
     :param plot: True == do a plot of the result
@@ -34,7 +35,7 @@ def fit(data_file_location: str, fit_function, params: Parameters, plot: bool, s
         return params
 
     # load in the data (expect it of the form x y yerr)
-    x, y, y_error = load_muon_data(data_file_location, start_time=start_time, end_time=end_time)
+    x, y, y_error = load_muon_data(muon_data, start_time=start_time, end_time=end_time)
 
     fitted_params = params
 
@@ -107,36 +108,82 @@ def residual(params, fit_function, x, y, yerr):
     return (y - y_func) / yerr
 
 
-def load_muon_data(filename: str, start_time=None, end_time=None, encoding='iso-8859-1'):
+def load_muon_data(muon_data: dict, start_time=None, end_time=None, encoding='iso-8859-1'):
     # loads muon data, spits it out as three numpy arrays: x, y, yerr
 
     if start_time is None:
         start_time = 0
-
-    # open the file
-    data_file = open(filename, 'r', encoding=encoding)
 
     # set up the arrays
     x = []
     y = []
     y_error = []
 
-    for line in data_file.readlines():
-        # if line doesn't start with !...
-        if not line.startswith('!'):
-            # split the line based on spaces
-            split_line = line.split()
-            # put these into the arrays
-            this_x = float(split_line[0])
-            if this_x < start_time:
-                continue
-            this_y = float(split_line[1])
-            this_yerror = float(split_line[2])
-            if end_time is not None:
-                if this_x > end_time:
-                    break
-            x.append(this_x)
-            y.append(this_y)
-            y_error.append(this_yerror)
+    # see what data we've been given
+    if 'asymmetry' in muon_data:
+        asym_file_location = muon_data['asymmetry']
+        # open the file
+        data_file = open(asym_file_location, 'r', encoding=encoding)
+        for line in data_file.readlines():
+            # if line doesn't start with !...
+            if not line.startswith('!'):
+                # split the line based on spaces
+                split_line = line.split()
+                # put these into the arrays
+                this_x = float(split_line[0])
+                if this_x < start_time:
+                    continue
+                this_y = float(split_line[1])
+                this_yerror = float(split_line[2])
+                if end_time is not None:
+                    if this_x > end_time:
+                        break
+                x.append(this_x)
+                y.append(this_y)
+                y_error.append(this_yerror)
+        data_file.close()
+        return np.array(x), np.array(y), np.array(y_error)
+    else:
+        # need to deal with N_F and N_B individually
+        n_f_file_location = muon_data['N_F']
+        n_b_file_location = muon_data['N_B']
+        alpha = muon_data['alpha']
 
-    return np.array(x), np.array(y), np.array(y_error)
+        # open both files:
+        n_f_file = open(n_f_file_location, 'r', encoding=encoding)
+        n_b_file = open(n_b_file_location, 'r', encoding=encoding)
+
+        # zip them both together, and iterate the lines
+        for data in list(zip(n_f_file.readlines(), n_b_file.readlines())):
+            if not data[0].startswith('!'):
+                # split both the n_f and n_b data
+                n_f = data[0].split()
+                n_b = data[1].split()
+                # check the times are the same,
+                assert abs(float(n_f[0]) - float(n_b[0])) < 1e-3
+                # save the time
+                this_x = float(n_f[0])
+                if this_x < start_time:
+                    continue
+                if end_time is not None:
+                    if this_x > end_time:
+                        break
+                x.append(this_x)
+                # save the asymmetry
+                n_f_counts = float(n_f[1])
+                n_b_counts = float(n_b[1])
+                try:
+                    asymmetry = 100*(n_b_counts - alpha*n_f_counts)/(n_b_counts + alpha*n_f_counts)
+                except ZeroDivisionError:
+                    x.pop(-1)
+                    continue
+                y.append(asymmetry)
+                err_n_f = float(n_f[2])
+                err_n_b = float(n_b[2])
+                asymmetry_error = 2*100*alpha/(n_b_counts + alpha*n_f_counts) ** 2 *\
+                                  np.sqrt((n_f_counts*err_n_b) ** 2 + (n_b_counts*err_n_f) ** 2)
+                y_error.append(asymmetry_error)
+        return np.array(x), np.array(y), np.array(y_error)
+
+
+
--- examples/CaF2_fitting/CaF2_fit.py
+++ examples/CaF2_fitting/CaF2_fit.py
@@ -37,12 +37,21 @@ def main():
     # set up the parameters
     params = Parameters()
     params.add('A', value=10)
-    params.add('r_nn', value=1.18, min=1, max=2)
-    params.add('lambda_squish', value=0.9, min=0.5, max=1)
-    params.add('A0', value=0, min=-5, max=5)
+    params.add('r_nn', value=1.172, min=1, max=2, vary=False)
+    params.add('field', value=20.85, vary=False)
+    params.add('lambda_squish', value=0.9, min=0.5, max=1, vary=False)
+    params.add('A0', value=10, min=0)
+
+    # data
+    muon_data = {#'asymmetry': './CaF2_data.dat',
+                 'N_F': './CaF2_T20_F.dat',
+                 'N_B': './CaF2_T20_B.dat',
+                 'alpha': 1.52,
+                 }
+
 
     # try to do a fit
-    fit(data_file_location='./CaF2_data.dat', end_time=15, fit_function=fit_function, params=params, plot=True)
+    fit(muon_data=muon_data, end_time=15, fit_function=fit_function, params=params, plot=True)
 
 
 # fit function
@@ -52,14 +61,16 @@ def fit_function(params, x):
     r_nn = params['r_nn']
     lambda_squish = params['lambda_squish']
     A0 = params['A0']
+    field = params['field']
 
     squish_radii = [r_nn]
 
     muon, All_spins = AO.get_linear_fmuf_atoms(ase_atoms=CaF2_atoms, muon_position=muon_position,
-                                               nnnness=10, squish_radii=squish_radii, lambda_squish=lambda_squish)
+                                               nnnness=3, squish_radii=squish_radii, lambda_squish=lambda_squish)
 
     deco = DipolarPolarisation.calc_dipolar_polarisation(all_spins=All_spins, muon=muon, muon_sample_polarisation=None, times=x,
-                                                         plot=False, shutup=True, gpu=True)
+                                                         plot=False, shutup=True, gpu=True, musr_type=DipolarPolarisation.musr_type.TF,
+                                                         field=field)
 
     return A*deco + A0*np.ones(len(x))
 
