396f0a255447e8992499e22f73f503e4d3318386
Johnny Wilkinson <johnnywilkinson12358@gmail.com>
2020-11-23 14:24:01 +0000

    made some examples for using muon_environment.py

---
+++ examples/CaF2_polarisation/CaF2_polarisation_env.py
@@ -0,0 +1,57 @@
+# Plot muon polarisation of CaF2 using muon_environment
+# works with Decoherence_calculator version>ff1a392386b66178c63dbe80dbfe6e7c5b935286
+
+import functools
+print = functools.partial(print, flush=True)
+
+import musr_decoherence.muon_environment as mu_env
+from musr_decoherence import DipolarPolarisation
+
+from ase.io import read
+import numpy as np
+
+
+def main():
+
+    cif_file = '../CaF2.cif'
+
+    # get the atoms from the CIF file
+    CaF2_atoms = read(cif_file)
+
+    # define the muon position as being in between atom 4 and 9 (which are both Fs in the structure).
+    nnindices = [4, 9]
+
+    # define how much the muon perturbs the environment. List is [[atom_id, dist], [atom_id, dist], ...]
+    perturbations = [[4, 1.18], [9, 1.18]]
+    # lambda_squish -- the factor by all the nearest-neighbours above what is
+    # defined in squish_radii are perturbed by (in most cases <1)
+    lambda_squish = 0.920
+
+    # add the muon to CaF2_atoms
+    CaF2_atoms_mu = mu_env.add_muon_to_aseatoms(CaF2_atoms, nn_indices=nnindices)
+
+    # now perturb the atoms
+    CaF2_atoms_mu = mu_env.perturb_atoms(CaF2_atoms_mu, perturbations=perturbations)
+
+    # get the dominant nuclei
+    nn_nuclei, _, _ = mu_env.get_dominant_nuclei(CaF2_atoms_mu)
+
+    # model the further nuclei with lambda_squish
+    nn_nuclei = mu_env.model_further_nuclei(nn_nuclei, draw_in_factor=lambda_squish)
+
+    muon, all_spins = mu_env.aseatoms_to_tdecoatoms(nn_nuclei)
+
+    output_file_name = '../../../CaF2_polarisation_T20.dat'
+
+    # define muon polarisation relative to the sample -- None for polycrystalline
+    muon_sample_polarisation = None
+
+    DipolarPolarisation.calc_dipolar_polarisation(all_spins=all_spins, muon=muon, muon_sample_polarisation=muon_sample_polarisation,
+                                                  plot=True, fourier=False, fourier_2d=False, tol=1e-3, gpu=True, shutup=False,
+                                                  times=np.arange(0, 20, 0.1), outfile_location=output_file_name,
+                                                  musr_type=DipolarPolarisation.musr_type.ZF)
+    return 0
+
+
+if __name__=='__main__':
+    main()
--- examples/NaF_polarisation/NaF_q_polarisation.py
+++ examples/NaF_polarisation/NaF_q_polarisation.py
@@ -3,41 +3,50 @@
 import functools
 print = functools.partial(print, flush=True)
 
+from ase.io import read
 from musr_decoherence import DipolarPolarisation
-from musr_decoherence.MDecoherenceAtom import TDecoherenceAtom as atom
-from musr_decoherence.TCoord3D import TCoord3D as coord
+from musr_decoherence import muon_environment as mu_env
 import numpy as np
-import NaF_EFG
-
 
 def main():
     r_nn = 1.19778
     r_nnn = 2.2926
     r_nnnn = 2.4153789
 
-    muon = atom(name='mu', position=coord(0, 0, 0))
+    cif_file = '../NaF.cif'
 
-    # calculate the EFG for the quadrupoles
-    efg = NaF_EFG.calc_efg(r_nnn, r_nn) / (2.31 ** 3)
+    # get the atoms from the CIF file
+    naf_atoms = read(cif_file)
 
-    All_spins = [muon,
-                 # nn
-                 atom(name='F', position=coord(1, -1, 0).rhat() * r_nn),
-                 atom(name='F', position=coord(-1, 1, 0).rhat() * r_nn),
-                 # nnn
-                 atom(name='Na', position=coord(1, 1, 0).rhat() * r_nnn, efg=efg),
-                 atom(name='Na', position=coord(-1, -1, 0).rhat() * r_nnn, efg=efg),
-                 # nnnn
-                 atom(name='F', position=coord(1, 1, 2).rhat() * r_nnnn),
-                 atom(name='F', position=coord(1, 1, -2).rhat() * r_nnnn),
-                 atom(name='F', position=coord(-1, -1, 2).rhat() * r_nnnn),
-                 atom(name='F', position=coord(-1, -1, -2).rhat() * r_nnnn),
-                 ]
+    # define the muon position as being in between atom 4 and 9 (which are both Fs in the structure).
+    nnindices = [5, 6]
 
+    # define how much the muon perturbs the environment. List is [[atom_id, dist], [atom_id, dist], ...]
+    perturbations = [[5, 1.18], [6, 1.18], [0, 2.2926], [3, 2.2926]]
+    # lambda_squish -- the factor by all the nearest-neighbours above what is
+    # defined in squish_radii are perturbed by (in most cases <1)
+    lambda_squish = 0.920
+
+    # add the muon to naf_atoms
+    naf_atoms_mu = mu_env.add_muon_to_aseatoms(naf_atoms, nn_indices=nnindices)
+
+    # now perturb the atoms
+    naf_atoms_mu = mu_env.perturb_atoms(naf_atoms_mu, perturbations=perturbations)
+
+    # get the dominant nuclei
+    nn_nuclei, naf_atoms_mu, nn_indices = mu_env.get_dominant_nuclei(naf_atoms_mu, unperturbed_atoms=naf_atoms)
+
+    # calculate quadrupoles
+    efgs, quad_nuc_ids = mu_env.calculate_quadrupoles(naf_atoms_mu, nn_indices, naf_atoms, 50)
+
+    # model the further nuclei with lambda_squish
+    nn_nuclei = mu_env.model_further_nuclei(nn_nuclei, draw_in_factor=lambda_squish)
+
+    muon, all_spins = mu_env.aseatoms_to_tdecoatoms(nn_nuclei, efgs=efgs, efg_ids=quad_nuc_ids)
     # file name
     output_file_name = 'NaF_polarisation.dat'
 
-    DipolarPolarisation.calc_dipolar_polarisation(all_spins=All_spins, muon=muon, times=np.arange(0, 20, 0.1), plot=True,
+    DipolarPolarisation.calc_dipolar_polarisation(all_spins=all_spins, muon=muon, times=np.arange(0, 20, 0.1), plot=True,
                                                   outfile_location=output_file_name, do_quadrupoles=True, gpu=True)
 
     return 0
