5e968c849befb689d74dcabd3d9f15de3252449d
Johnny Wilkinson <johnnywilkinson12358@gmail.com>
2021-02-09 17:40:59 +0000

    fixed PercolationPolarisation bugs

--- musr_decoherence/PercolationPolarisation.py
+++ musr_decoherence/PercolationPolarisation.py
@@ -21,6 +21,7 @@ def calc_equiv_percolated_polarisation(all_spins:list, muon: atom, p: float, tim
     :param times: np.ndarray of the times to calculate for
     :param gpu: if True, uses GPU parallelisation
     :param plot: if True, does a plot at the end
+    :return: np.ndarray of the average muon polarisation.
     """
 
     # count the number of sites (i.e 1 less than all_spins, as all_spins contains the muon)
@@ -32,18 +33,20 @@ def calc_equiv_percolated_polarisation(all_spins:list, muon: atom, p: float, tim
 
     # for each site
     for n_vacancy in range(1, n_sites):
-        these_spins = all_spins[:,-n_vacancy]
+        # remove n_vacancy spins from the array all_spins
+        these_spins = all_spins[:-n_vacancy]
+        # calculate the probability that this number of sites are not occupied
         probability = (1 - p) ** (n_sites - n_vacancy) * p ** n_vacancy
-        print(probability)
+        # now multiply that probability by the number of combinations of that type of site
         probability *= math.factorial(n_sites) / (math.factorial(n_sites - n_vacancy)* math.factorial(n_vacancy))
-        print(probability)
-        result += probability * DipolarPolarisation.calc_dipolar_polarisation(all_spins=all_spins, muon=muon,
+        # now add this polarisation onto the final result
+        result += probability * DipolarPolarisation.calc_dipolar_polarisation(all_spins=these_spins, muon=muon,
                                                                               times=times, shutup=True, gpu=gpu)
 
-    # add on all vacancies
+    # add on all vacancies (i.e flat line...)
     result += p ** n_sites * np.ones(shape=times.shape)
 
     pyplot.plot(times, result)
-    pyplot.plot.show()
+    pyplot.show()
 
     return result
