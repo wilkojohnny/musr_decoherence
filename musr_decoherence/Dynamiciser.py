1504615eec801636f5f0ec00cbcb40589606a712
John Wilkinson <john.wilkinson@stfc.ac.uk>
2024-05-13 16:31:52 +0100

    Added a Muonium radical -> mu+ function in dynamiciser.py

--- musr_decoherence/Dynamiciser.py
+++ musr_decoherence/Dynamiciser.py
@@ -1,24 +1,25 @@
 """
 Dynamiciser.py -- adds dynamics to any MuSR polarisation function
 By John Wilkinson, 6/10/2020
-git test
+13/5/2024 -- added in radical->mu+
 """
 
 import numpy as np
 from matplotlib import pyplot as plt
-
+from typing import Callable
+from scipy import integrate
 
 def main():
 
-    dt = 0.1
-    t = np.arange(0, 10, dt)
-    static_function = 1/3*np.ones(t.shape) + 2/3*(1-t**2)*np.exp(-.5*(t**2))
+    delta = 0.3
+    KT = lambda x: 1/3 + 2/3 * (1-(delta*x) ** 2) * np.exp(-(delta * x) ** 2 / 2)
 
-    nu = 0.1
+    nu = 0.01
 
-    dynamic_function = dynamicise(static_function, t, dt, nu)
+    t = np.arange(0.0, 35.0, 0.01)
 
-    plt.plot(t, static_function)
+    dynamic_function = radical_to_muplus(muplus_function=KT, radical_lambda=0.005, nu=nu, t=t)
+
     plt.plot(t, dynamic_function)
     plt.show()
 
@@ -36,6 +37,13 @@ def dynamicise(static_function: np.ndarray, t: np.ndarray, dt: float, nu:float) 
     dynamic_function = np.zeros(static_function.shape)
 
     # start the dynamic function at 1
+    remove_extra = False
+    if t[0] != 0:
+        t = np.insert(t, 0, 0)
+        dynamic_function = np.insert(dynamic_function,0, 0)
+        static_function = np.insert(static_function, 0, 1)
+        remove_extra = True
+
     dynamic_function[0] = 1
 
     for i_t in range(1, len(t)):
@@ -46,7 +54,31 @@ def dynamicise(static_function: np.ndarray, t: np.ndarray, dt: float, nu:float) 
         sum *= nu*dt
         dynamic_function[i_t] = 1/(1 - nu*dt/2) * (static_function[i_t]*(1 + nu*dt/2)*np.exp(-nu*this_t) + sum)
 
+    if remove_extra:
+        dynamic_function = np.delete(dynamic_function, 0)
+
     return dynamic_function
+
+
+def radical_to_muplus(muplus_function: Callable[[np.ndarray], np.ndarray], radical_lambda: float, nu: float,
+                      t: np.ndarray) -> np.ndarray:
+    """
+    Calculates the polarization function for a muon starting off in a muonium radical state (with polarisation function
+    exp(-lambda*t), and then hops with rate nu into a mu+ polarization function (which could be e.g. Kubo-Toyabe).
+    :param muplus_function: function of the form F(t), where t is a numpy array, and returns a numpy array.
+    :param radical_lambda: decay rate of the radical state
+    :param nu: hop rate from radical -> muplus_function
+    :param t: time
+    :return: numpy array of the polarization function at the times t
+    """
+    result = np.zeros(t.shape)
+    for i_t, this_t in enumerate(t):
+        integral = integrate.quad(lambda int_time: muplus_function(this_t - int_time) *
+                                                   np.exp(-(nu + radical_lambda)*int_time),
+                                  0, this_t)
+        result[i_t] = integral[0]
+
+    return np.exp(-(radical_lambda + nu) * t) + nu*result
 
 
 if __name__=='__main__':
